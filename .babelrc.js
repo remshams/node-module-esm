305b24cd4271cb095041edad9bd6130828b3cf3e
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-02-05 19:28:27 +0100

    * adds sharedConfig for babelrc * updates package.json script nanme for building esm artifact

--- .babelrc.js
+++ .babelrc.js
@@ -1,14 +1,24 @@
+const shared = {
+  ignore: ['src/**/*.spec.ts']
+}
 const sharedPresets = ['@babel/typescript'];
 
 module.exports = {
   env: {
-    es: {
+    esmUnbundled: {
+      ...shared,
       presets: [['@babel/env', {
         modules: false
       }], ...sharedPresets],
-      ignore: ['src/**/*.spec.ts']
+    },
+    esmBundled: {
+      ...shared,
+      presets: [['@babel/env', {
+        targets: "> 0.25%, not dead"
+      }], ...sharedPresets],
     },
     cjs: {
+      ...shared,
       presets: ['@babel/env', ...sharedPresets],
       ignore: ['src/**/*.spec.ts']
     },
