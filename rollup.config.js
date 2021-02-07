36f7f29377cfa4caba4e03fc48ba3825ce6364f0
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-02-07 09:28:05 +0100

    * updates umd "name" property in rollup.config.js * reformats rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -6,28 +6,32 @@ const extensions = ['.js', '.ts' ];
 
 export default  {
   input: 'src/index.ts',
-  output: [{
-    file: 'lib/bundles/bundle.esm.js',
-    format: 'esm',
-    name: 'bundle.esm',
-    sourcemap: true
-  },{
-    file: 'lib/bundles/bundle.esm.min.js',
-    format: 'esm',
-    plugins: [terser()],
-    sourcemap: true
-  }, {
-    file: 'lib/bundles/bundle.umd.js',
-    format: 'umd',
-    name: 'bundle',
-    sourcemap: true
-  }, {
-    file: 'lib/bundles/bundle.umd.min.js',
-    format: 'umd',
-    name: 'bundle',
-    plugins: [terser()],
-    sourcemap: true
-  }],
+  output: [
+    {
+      file: 'lib/bundles/bundle.esm.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      file: 'lib/bundles/bundle.esm.min.js',
+      format: 'esm',
+      plugins: [terser()],
+      sourcemap: true
+    },
+    {
+      file: 'lib/bundles/bundle.umd.js',
+      format: 'umd',
+      name: 'myLibrary',
+      sourcemap: true
+    },
+    {
+      file: 'lib/bundles/bundle.umd.min.js',
+      format: 'umd',
+      name: 'myLibrary',
+      plugins: [terser()],
+      sourcemap: true
+    }
+  ],
   plugins: [
     resolve({ extensions }),
     babel({ babelHelpers: 'bundled', include: ['src/**/*.ts'], extensions, exclude: './node_modules/**'})
